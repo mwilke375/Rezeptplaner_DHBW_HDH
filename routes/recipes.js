042297ad523836bbc47fafd16c08133d638c29b6
mwilke375 <mika.wilke@icloud.com>
2026-02-24 20:43:00 +0100

    delete & search

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Recipe = require('../models/Recipe');
 
+// US01 & US03: Rezept erstellen
 router.post('/', async (req, res) => {
   try {
     const newRecipe = new Recipe(req.body);
@@ -12,10 +13,26 @@ router.post('/', async (req, res) => {
   }
 });
 
+// US09: Rezepte abrufen & Suchen
 router.get('/', async (req, res) => {
   try {
-    const recipes = await Recipe.find();
+    const query = {};
+    if (req.query.search) {
+      query.title = { $regex: req.query.search, $options: 'i' };
+    }
+    const recipes = await Recipe.find(query);
     res.status(200).json(recipes);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// US04: Rezept löschen
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedRecipe = await Recipe.findByIdAndDelete(req.params.id);
+    if (!deletedRecipe) return res.status(404).json({ message: 'Nicht gefunden' });
+    res.status(200).json({ message: 'Rezept erfolgreich gelöscht' });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
